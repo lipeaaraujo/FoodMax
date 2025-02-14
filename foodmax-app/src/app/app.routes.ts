befbcb3601f7b53ea2c2bfa7baba8dee9c5549cc
LipeAKugel <felipearaujodff@gmail.com>
2025-02-13 22:18:29 -0300

    feat: creating login component

--- foodmax-app/src/app/app.routes.ts
+++ foodmax-app/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home.component';
 import { RecipeDetails } from './components/recipedetails.component';
+import { LoginComponent } from './components/login.component';
 
 export const routes: Routes = [
-  { path: "", component: HomeComponent, title: "Home" },
+  { path: "", redirectTo: "/login", pathMatch: "full" },
+  { path: "login", component: LoginComponent, title: "Login" },
+  { path: "home", component: HomeComponent, title: "Home" },
   { path: "recipe-details/:id", component: RecipeDetails, title: "Recipe Details" }
 ];
---
+++ foodmax-app/src/app/components/login.component.ts
@@ -0,0 +1,61 @@
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { Router, RouterModule } from "@angular/router";
+
+@Component({
+  selector: "login-component",
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    RouterModule,
+    ReactiveFormsModule
+  ],
+  template: `
+    <section class="login-container">
+      <form [formGroup]="loginForm" (ngSubmit)="handleSubmit()">
+        <h2>Login</h2>  
+        <mat-form-field>
+          <mat-label>Username</mat-label>
+          <input 
+            matInput
+            type="text"
+            required
+          />
+        </mat-form-field>
+        <mat-form-field>
+          <mat-label>Password</mat-label>
+          <input 
+            matInput
+            type="password"
+            required
+          />
+        </mat-form-field>
+        <button routerLink="/home" mat-flat-button>
+          Login
+        </button>
+        <a routerLink="/register" mat-button>
+          Create Account
+        </a>
+      </form>
+    </section>
+  `,
+  styleUrl: "../styles/login.component.css",
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  })
+
+  constructor(private router: Router) {}
+
+  handleSubmit(): void {
+    this.router.navigate(["/home"])
+  }
+}
